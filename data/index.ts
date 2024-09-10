2fa9079ee579b0137ed76ce1a20b7572c6bd8839
Adrian Pruszkowski <pruszkowski.adrian@gmail.com>
2024-09-10 14:12:06 +0200

    first commit

---
+++ data/index.ts
@@ -0,0 +1,206 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Zego Website",
+    des: "Motor Insurance · Drive well, save big · Car, scooter and motorbike delivery insurance · Delivery insurance that works around you",
+    img: "/zeg2.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/sanity.svg"],
+    link: "https://www.zego.com/",
+  },
+  {
+    id: 2,
+    title: "Trailfinders",
+    des: "Discover the world with Trailfinders, the Travel Experts. Luxury hotels, honeymoons, family trips, private tours, last minute holidays and much more",
+    img: "/tra7.png",
+    iconLists: ["/vue.svg", "/tail.svg", "/ts.svg", "/re.svg"],
+    link: "https://www.trailfinders.com/",
+  },
+  {
+    id: 3,
+    title: "London Design",
+    des: "Freelance London Design agenty to create your dream website",
+    img: "/lon2.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg"],
+    link: "the-london-designs-2fyoxpahq-adrian23221.vercel.app",
+  },
+  {
+    id: 4,
+    title: "Weather App",
+    des: "Quick and simple Weather checker made in React JS",
+    img: "/wea.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "/https://adrian23221.github.io/WeatherApp/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Dolores",
+    title: "Owner of Dolores Tailor",
+  },
+  {
+    quote:
+      "I’m beyond impressed with the website you created for my business! It perfectly captures the brand and delivers a fantastic user experience. Your creativity, professionalism, and ability to bring ideas to life are truly remarkable. You went above and beyond, and the final product is better than I could have imagined. Thank you for your exceptional work!",
+    name: "Joanna Rostek",
+    title: "Owner of Dla Zdrowia Mozgu",
+  },
+  {
+    quote:
+      "Thank you so much for the amazing work on my business website! It's exactly what I envisioned—clean, professional, and easy to navigate. Your attention to detail and responsiveness throughout the process made it a smooth and enjoyable experience. I'm thrilled with the result and already getting great feedback from visitors. I highly recommend your services!",
+    name: "Adek",
+    title: "Owner of London Design",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    desc: "Assisted in the development of a web-based platform using JavaScript, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/fib.png",
+  },
+  {
+    id: 2,
+    title: "Frontend Developer",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React.js.",
+    className: "md:col-span-2",
+    thumbnail: "/gnm3.png",
+  },
+  {
+    id: 3,
+    title: "Web Developer",
+    desc: "Implemented UI/UX Design for our pages. Optimalise and create strategy for SEO perspective",
+    className: "md:col-span-2",
+    thumbnail: "/trf.png",
+  },
+  {
+    id: 4,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp5.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/adrian-pruszkowski/",
+  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  // },
+  // {
+  //   id: 1,
+  //   img: "/link.svg",
+  // },
+];
