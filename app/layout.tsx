cad2391501276488741ee756f1f767d5538ef76e
Adrian Pruszkowski <pruszkowski.adrian@gmail.com>
2024-09-10 16:03:01 +0200

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -8,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Modern & Minimal Next JS Portfolio",
 };
 
 export default function RootLayout({
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <GoogleTagManager gtmId="G-NG7M7WM3MF" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
@@ -28,6 +30,14 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=G-NG7M7WM3MF"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           {children}
         </ThemeProvider>
       </body>
